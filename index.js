e9418031c3de134dd5d404c961814a3031aa679d
Johann Lozada <johannlozada@gmail.com>
2023-06-21 12:52:26 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/* Borrar logo de carga */
+window.addEventListener('load', function() {
+  const loadingScreen = document.querySelector('.cargar');
+  for (var i = 1; i <= 8; i++) {
+    setTimeout(function() {
+      if (i === 9) {
+        loadingScreen.style.display = 'none';
+      }
+    }, i * 1000);
+  }
+});
+/* Desactivar click derecho del mouse */
+/*
+document.addEventListener("contextmenu", function(event) {
+  event.preventDefault();
+});
+*/
+/* Lista de variantes */
+
+
+
+
